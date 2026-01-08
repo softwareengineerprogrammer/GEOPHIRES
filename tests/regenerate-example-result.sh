f80ed0b743fbb795a16818cb667bf497267a4ea4
softwareengineerprogrammer <4056124+softwareengineerprogrammer@users.noreply.github.com>
2026-01-08 11:14:07 -0800

    WIP - calibrated initial production temperature and drawdown at year 15 (need to recalibrate to 3:2 producer:injector ratio)

--- tests/regenerate-example-result.sh
+++ tests/regenerate-example-result.sh
@@ -27,7 +27,7 @@ then
     echo "Regenerating Fervo_Project_Cape-5..."
     # TODO synchronize replacement matching with Fervo_Project_Cape-4 values
     sed -e 's/Construction Years, 5/Construction Years, 3/' \
-        -e 's/Number of Doublets, 52/Number of Doublets, 11/' \
+        -e 's/Number of Doublets, 50/Number of Doublets, 10/' \
         -e 's/500 MWe/100 MWe/' \
         -e 's/Phase II/Phase I/' \
         examples/Fervo_Project_Cape-4.txt > examples/Fervo_Project_Cape-5.txt
