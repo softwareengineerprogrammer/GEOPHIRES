8db1647028c9c0da33d49e75aabbb6bb579586ab
softwareengineerprogrammer <4056124+softwareengineerprogrammer@users.noreply.github.com>
2026-01-13 11:40:34 -0800

    fix incorrect version reference in geophires_docs/__init__.py

--- src/geophires_docs/__init__.py
+++ src/geophires_docs/__init__.py
@@ -4,4 +4,4 @@ from pathlib import Path
 
 _PROJECT_ROOT: Path = Path(__file__).parent.parent.parent
 _FPC5_INPUT_FILE_PATH: Path = _PROJECT_ROOT / 'tests/examples/Fervo_Project_Cape-5.txt'
-_FPC5_RESULT_FILE_PATH: Path = _PROJECT_ROOT / 'tests/examples/Fervo_Project_Cape-4.out'
+_FPC5_RESULT_FILE_PATH: Path = _PROJECT_ROOT / 'tests/examples/Fervo_Project_Cape-5.out'
