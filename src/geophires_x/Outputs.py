e28fe36c03969bb65ab2699710fca2801dc6104a
softwareengineerprogrammer <4056124+softwareengineerprogrammer@users.noreply.github.com>
2026-01-14 10:31:54 -0800

    fix incorrect newlines in output that created diff noise pollution

--- src/geophires_x/Outputs.py
+++ src/geophires_x/Outputs.py
@@ -346,15 +346,15 @@ class Outputs:
                 f.write(NL)
                 f.write('                         ***RESOURCE CHARACTERISTICS***\n')
                 f.write(NL)
-                f.write(f'      Maximum reservoir temperature:                   {model.reserv.Tmax.value:10.1f} ' + model.reserv.Tmax.CurrentUnits.value + NL)
-                f.write(f'      Number of segments:                            {model.reserv.numseg.value:10.0f} ' + NL)
+                f.write(f'      Maximum reservoir temperature:                   {model.reserv.Tmax.value:10.1f} {model.reserv.Tmax.CurrentUnits.value}\n')
+                f.write(f'      Number of segments:                            {model.reserv.numseg.value:10.0f}\n')
                 if model.reserv.numseg.value == 1:
-                    f.write(f'      Geothermal gradient:                                {model.reserv.gradient.value[0]:10.4g} ' + model.reserv.gradient.CurrentUnits.value + NL)
+                    f.write(f'      Geothermal gradient:                                {model.reserv.gradient.value[0]:10.4g} {model.reserv.gradient.CurrentUnits.value}\n')
                 else:
                     for i in range(1, model.reserv.numseg.value):
-                        f.write(f'      Segment {str(i):s}   Geothermal gradient:                    {model.reserv.gradient.value[i-1]:10.4g} ' + model.reserv.gradient.CurrentUnits.value +NL)
+                        f.write(f'      Segment {str(i):s}   Geothermal gradient:                    {model.reserv.gradient.value[i-1]:10.4g} {model.reserv.gradient.CurrentUnits.value}\n')
                         f.write(f'      Segment {str(i):s}   Thickness:                         {round(model.reserv.layerthickness.value[i-1], 10)} {model.reserv.layerthickness.CurrentUnits.value}\n')
-                    f.write(f'      Segment {str(i+1):s}   Geothermal gradient:                    {model.reserv.gradient.value[i]:10.4g} ' + model.reserv.gradient.CurrentUnits.value + NL)
+                    f.write(f'      Segment {str(i+1):s}   Geothermal gradient:                    {model.reserv.gradient.value[i]:10.4g} {model.reserv.gradient.CurrentUnits.value}\n')
 
                 f.write(NL)
                 f.write(NL)
