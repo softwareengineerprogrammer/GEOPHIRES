e6359b724cfebb27e24aa5c977ad8a00a3a60b66
softwareengineerprogrammer <4056124+softwareengineerprogrammer@users.noreply.github.com>
2026-01-16 09:56:27 -0800

    another attempt to fix path-related GHA failure (https://github.com/softwareengineerprogrammer/GEOPHIRES/actions/runs/21074210581/job/60611745150)

--- src/geophires_docs/__init__.py
+++ src/geophires_docs/__init__.py
@@ -1,7 +1,13 @@
 from __future__ import annotations
 
+import os
 from pathlib import Path
 
-_PROJECT_ROOT: Path = Path(__file__).parent.parent.parent
+
+def _get_file_path(file_name) -> Path:
+    return Path(os.path.join(os.path.abspath(os.path.dirname(__file__)), file_name))
+
+
+_PROJECT_ROOT: Path = _get_file_path('../..')
 _FPC5_INPUT_FILE_PATH: Path = _PROJECT_ROOT / 'tests/examples/Fervo_Project_Cape-5.txt'
 _FPC5_RESULT_FILE_PATH: Path = _PROJECT_ROOT / 'tests/examples/Fervo_Project_Cape-5.out'
