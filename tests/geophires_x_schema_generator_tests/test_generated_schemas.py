93a5edd29a6d00cf2740485dea108010fbc7469b
softwareengineerprogrammer <4056124+softwareengineerprogrammer@users.noreply.github.com>
2026-01-30 09:06:08 -0800

    test src schema min/max validity (inf not allowed) per TODO in https://github.com/NatLabRockies/GEOPHIRES-X/pull/457 re: https://github.com/NatLabRockies/GEOPHIRES-X/commit/7c13a1f19a0d068b2abe5b38eb605f243b4d3710

--- tests/geophires_x_schema_generator_tests/test_generated_schemas.py
+++ tests/geophires_x_schema_generator_tests/test_generated_schemas.py
@@ -1,4 +1,5 @@
 import json
+import math
 import tempfile
 from pathlib import Path
 
@@ -8,7 +9,7 @@ from geophires_x_schema_generator.main import generate_schemas
 
 class GeneratedSchemasTestCase(BaseTestCase):
 
-    def test_generated_schemas_up_to_date(self) -> None:
+    def test_generated_schemas_are_valid_and_up_to_date(self) -> None:
         try:
             build_dir: Path = Path(tempfile.gettempdir())
             generate_schemas(False, build_dir)
@@ -23,6 +24,11 @@ class GeneratedSchemasTestCase(BaseTestCase):
                 with open(src_geophires_request_path, encoding='utf-8') as f:
                     src_geophires_request_schema = json.loads(f.read())
 
+                    for _, schema_prop_entry in src_geophires_request_schema['properties'].items():
+                        for inf_not_allowed_prop in ['minimum', 'maximum']:
+                            if schema_prop_entry.get(inf_not_allowed_prop) is not None:
+                                self.assertFalse(math.isinf(float(schema_prop_entry[inf_not_allowed_prop])))
+
                 self.assertDictEqual(generated_geophires_request_schema, src_geophires_request_schema)
 
             assert_schema('geophires-request.json')
@@ -30,6 +36,6 @@ class GeneratedSchemasTestCase(BaseTestCase):
             assert_schema('hip-ra-x-request.json')
         except AssertionError as ae:
             raise AssertionError(
-                'Generated schemas in source are not up-to-date. '
+                'Generated schemas in source are invalid or not up-to-date. '
                 'Run src/geophires_x_schema_generator/main.py to update them.'
             ) from ae
