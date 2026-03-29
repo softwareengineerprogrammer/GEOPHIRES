softwareengineerprogrammer/GEOPHIRES

commit 6df5a630d9b9f6718075bfe5e450987ce15fbe1d

address backfill_lppa_metrics FIXME WIP - set LPPA to NaN for direct-use heat

src/geophires_x/EconomicsSam.py
@@ -461,22 +461,22 @@ class SamEconomicsCalculations:
             # ]
             # ret[_get_row_index('PPA price (cents/kWh)')][1:] = ppa_revenue_all_years
 
-            # Note: expected to be same in all pre-revenue years since both price and revenue are zero until COD
-            first_year_lppa_cents_per_kwh = (
-                first_year_pv_of_ppa_revenue_usd * 100.0 / ret[_get_row_index(pv_of_annual_energy_row_name)][1]
-            )
+            first_year_lppa_cents_per_kwh: float | str = 'NaN'
+            first_year_pv_annual_energy = ret[_get_row_index(pv_of_annual_energy_row_name)][1]
+
+            if isinstance(first_year_pv_annual_energy, float) and first_year_pv_annual_energy != 0.0:
+                # Note: expected to be same in all pre-revenue years since both price and revenue are zero until COD
+                first_year_lppa_cents_per_kwh = round(
+                    first_year_pv_of_ppa_revenue_usd * 100.0 / first_year_pv_annual_energy, 2
+                )
 
             lppa_row_name = 'LPPA Levelized PPA price nominal (cents/kWh)'
             ret[_get_row_index(lppa_row_name)][1:] = [
-                round(first_year_lppa_cents_per_kwh, 2),
+                first_year_lppa_cents_per_kwh,
                 *([None] * self._pre_revenue_years_count),
             ]
 
-        try:
-            backfill_lppa_metrics()
-        except ZeroDivisionError:
-            # FIXME WIP - indicates heat only end-use/surface application
-            pass
+        backfill_lppa_metrics()
 
         return ret
 

tests/examples/example_SAM-single-owner-PPA-8_heat.out
@@ -6,8 +6,8 @@ Simulation Metadata
 ----------------------
  GEOPHIRES Version: 3.12.1
  Simulation Date: 2026-03-29
- Simulation Time:  08:06
- Calculation Time:      1.802 sec
+ Simulation Time:  08:11
+ Calculation Time:      1.793 sec
 
                            ***SUMMARY OF RESULTS***
 
@@ -346,7 +346,7 @@ LCOE Levelized cost of energy nominal (cents/kWh)	NaN
 
 Present value of PPA revenue ($)                 	0
 Present value of annual energy nominal (kWh)     	0
-LPPA Levelized PPA price nominal (cents/kWh)     	           	            	0.0
+LPPA Levelized PPA price nominal (cents/kWh)     	NaN
 
 PROJECT STATE INCOME TAXES
 EBITDA ($)                                       	           	            	0           	50,019,763   	50,223,211   	50,285,159   	50,321,788   	50,347,375   	50,364,797   	50,347,219   	50,105,185   	49,899,383   	50,198,604   	50,273,076   	50,313,959   	50,341,698   	50,361,571   	50,360,009   	50,211,686   	49,845,945   	50,165,867   	50,259,175   	50,305,357   	50,335,576   	50,357,405   	50,365,469   	50,280,457   	49,858,708   	50,116,858   	50,242,865   	50,295,826   	50,328,963   	172,518,492

commit 6f149d6b59df400fb2b4726b7bba39d0c5d0820a

fix backfill_lppa_metrics (error introduced in 6df5a630d9b9f6718075bfe5e450987ce15fbe1d). add SurfacePlant NetElectricityProducedMax derived output parameter

src/geophires_x/Economics.py
@@ -3640,20 +3640,10 @@ class Economics:
         self.capex_total.value = (self.sam_economics_calculations.capex.quantity()
                                   .to(self.capex_total.CurrentUnits.value).magnitude)
 
-        # TODO define this as an output of SurfacePlant rather than calculating it on-demand here and elsewhere
-        max_net_electricity_generation_kw = quantity(
-            np.max(model.surfaceplant.NetElectricityProduced.value),
-            model.surfaceplant.NetElectricityProduced.CurrentUnits
-        ).to('kW')
-        try:
+        if model.surfaceplant.enduse_option.value.has_electricity_component:
+            max_net_electricity_generation_kw = model.surfaceplant.NetElectricityProducedMax.quantity().to('kW')
             capex_total_per_kw_q = self.capex_total.quantity().to('USD') / max_net_electricity_generation_kw
             self.capex_total_per_kw.value = capex_total_per_kw_q.magnitude
-        except Exception as e: # RuntimeError as re:
-            # FIXME WIP
-            if str(e) == 'divide by zero encountered in scalar divide':
-                self.capex_total_per_kw.value = float('inf')
-            else:
-                raise e
 
         self.CCap.value = (self.sam_economics_calculations.capex.quantity()
                            .to(self.CCap.CurrentUnits.value).magnitude)

src/geophires_x/EconomicsSam.py
@@ -464,7 +464,9 @@ class SamEconomicsCalculations:
             first_year_lppa_cents_per_kwh: float | str = 'NaN'
             first_year_pv_annual_energy = ret[_get_row_index(pv_of_annual_energy_row_name)][1]
 
-            if isinstance(first_year_pv_annual_energy, float) and first_year_pv_annual_energy != 0.0:
+            if (
+                isinstance(first_year_pv_annual_energy, int) or isinstance(first_year_pv_annual_energy, float)
+            ) and first_year_pv_annual_energy != 0.0:
                 # Note: expected to be same in all pre-revenue years since both price and revenue are zero until COD
                 first_year_lppa_cents_per_kwh = round(
                     first_year_pv_of_ppa_revenue_usd * 100.0 / first_year_pv_annual_energy, 2

src/geophires_x/Outputs.py
@@ -222,7 +222,8 @@ class Outputs:
 
                 if is_sam_econ_model:
                     f.write(f'      {Outputs._field_label(econ.capex_total.display_name, 50)}{econ.capex_total.value:10.2f} {econ.capex_total.CurrentUnits.value}\n')
-                    f.write(f'      {Outputs._field_label(econ.capex_total_per_kw.display_name, 50)}{econ.capex_total_per_kw.value:10.0f} {econ.capex_total_per_kw.CurrentUnits.value}\n')
+                    if model.surfaceplant.enduse_option.value.has_electricity_component:
+                        f.write(f'      {Outputs._field_label(econ.capex_total_per_kw.display_name, 50)}{econ.capex_total_per_kw.value:10.0f} {econ.capex_total_per_kw.CurrentUnits.value}\n')
 
                 f.write(f'      Number of production wells:                    {model.wellbores.nprod.value:10.0f}'+NL)
                 f.write(f'      Number of injection wells:                     {model.wellbores.ninj.value:10.0f}'+NL)
@@ -597,17 +598,17 @@ class Outputs:
                 else:
                     f.write(f'      {econ.Coam.display_name}:            {econ.Coam.value:10.2f} {econ.Coam.CurrentUnits.value}\n')
 
-                f.write(NL)
-                f.write(NL)
-                f.write('                           ***SURFACE EQUIPMENT SIMULATION RESULTS***\n')
-                f.write(NL)
-                if model.surfaceplant.enduse_option.value in [EndUseOptions.ELECTRICITY, EndUseOptions.COGENERATION_TOPPING_EXTRA_HEAT, EndUseOptions.COGENERATION_TOPPING_EXTRA_ELECTRICITY, EndUseOptions.COGENERATION_BOTTOMING_EXTRA_ELECTRICITY, EndUseOptions.COGENERATION_BOTTOMING_EXTRA_HEAT, EndUseOptions.COGENERATION_PARALLEL_EXTRA_HEAT, EndUseOptions.COGENERATION_PARALLEL_EXTRA_ELECTRICITY]: # there is an electricity componenent:
+                f.write('\n\n                           ***SURFACE EQUIPMENT SIMULATION RESULTS***\n')
+
+                if model.surfaceplant.enduse_option.value.has_electricity_component:
+                    sp = model.surfaceplant
+                    # FIXME switch to CurrentUnits instead of PreferredUnits
                     f.write(f'      Initial geofluid availability:                    {model.surfaceplant.Availability.value[0]:10.2f} ' + model.surfaceplant.Availability.PreferredUnits.value + NL)
                     f.write(f'      Maximum Total Electricity Generation:             {np.max(model.surfaceplant.ElectricityProduced.value):10.2f} ' + model.surfaceplant.ElectricityProduced.PreferredUnits.value + NL)
                     f.write(f'      Average Total Electricity Generation:             {np.average(model.surfaceplant.ElectricityProduced.value):10.2f} ' + model.surfaceplant.ElectricityProduced.PreferredUnits.value + NL)
                     f.write(f'      Minimum Total Electricity Generation:             {np.min(model.surfaceplant.ElectricityProduced.value):10.2f} ' + model.surfaceplant.ElectricityProduced.PreferredUnits.value + NL)
                     f.write(f'      Initial Total Electricity Generation:             {model.surfaceplant.ElectricityProduced.value[0]:10.2f} ' + model.surfaceplant.ElectricityProduced.PreferredUnits.value + NL)
-                    f.write(f'      Maximum Net Electricity Generation:               {np.max(model.surfaceplant.NetElectricityProduced.value):10.2f} ' + model.surfaceplant.NetElectricityProduced.PreferredUnits.value + NL)
+                    f.write(f'      {sp.NetElectricityProducedMax.display_name}:               {sp.NetElectricityProducedMax.value:10.2f} {sp.NetElectricityProducedMax.CurrentUnits.value}\n')
                     f.write(f'      Average Net Electricity Generation:               {np.average(model.surfaceplant.NetElectricityProduced.value):10.2f} ' + model.surfaceplant.NetElectricityProduced.PreferredUnits.value + NL)
                     f.write(f'      Minimum Net Electricity Generation:               {np.min(model.surfaceplant.NetElectricityProduced.value):10.2f} ' + model.surfaceplant.NetElectricityProduced.PreferredUnits.value + NL)
                     f.write(f'      Initial Net Electricity Generation:               {model.surfaceplant.NetElectricityProduced.value[0]:10.2f} ' + model.surfaceplant.NetElectricityProduced.PreferredUnits.value + NL)

src/geophires_x/SurfacePlant.py
@@ -550,6 +550,13 @@ class SurfacePlant:
             PreferredUnits=PowerUnit.MW,
             CurrentUnits=PowerUnit.MW
         )
+        self.NetElectricityProducedMax = self.OutputParameterDict[self.NetElectricityProducedMax.Name] = OutputParameter(
+            Name="Maximum Net Electricity Generation",
+            UnitType=Units.POWER,
+            PreferredUnits=PowerUnit.MW,
+            CurrentUnits=PowerUnit.MW
+        )
+
         self.TotalkWhProduced = self.OutputParameterDict[self.TotalkWhProduced.Name] = OutputParameter(
             Name="Total Electricity Generation",
             UnitType=Units.ENERGY,
@@ -752,3 +759,6 @@ class SurfacePlant:
                 self.heat_to_power_conversion_efficiency.value = avg_efficiency
 
         self.enduse_option_output.value = self.enduse_option.value.value
+
+        self.NetElectricityProducedMax.value = np.max(self.NetElectricityProduced.quantity()).to(
+            self.NetElectricityProducedMax.CurrentUnits).magnitude