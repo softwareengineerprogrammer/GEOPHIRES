c8125749f8895f3ef03d2bdb87f25eb91710cf16
softwareengineerprogrammer <4056124+softwareengineerprogrammer@users.noreply.github.com>
2026-04-01 09:24:48 -0700

    calculate LCOE based on PV of annual energy (electricity only) costs

--- src/geophires_x/EconomicsSamCalculations.py
+++ src/geophires_x/EconomicsSamCalculations.py
@@ -354,17 +354,19 @@ class SamEconomicsCalculations:
             *([''] * (self._pre_revenue_years_count - 1)),
         ]
 
-        if self.electricity_plant_frac_of_capex < 1.0:
-            # WIP - maybe should always include this?
-            pv_of_annual_energy_costs_row_name = 'Present value of annual energy costs ($)'
-            # pv_of_annual_energy_costs_row_index = _get_row_index(pv_of_annual_costs_row_name)
-            _insert_row_before(
-                'Present value of annual energy nominal (kWh)',
-                pv_of_annual_energy_costs_row_name,
-                [
-                    pv_of_annual_costs_backfilled[0] * self.electricity_plant_frac_of_capex,
-                ],
-            )
+        pv_of_annual_energy_costs_row_name = 'Present value of annual energy costs ($)'
+        pv_of_annual_energy_costs_at_year_0_usd = (
+            pv_of_annual_costs_backfilled[0] * self.electricity_plant_frac_of_capex
+        )
+        # pv_of_annual_energy_costs_row_index = _get_row_index(pv_of_annual_costs_row_name)
+        _insert_row_before(
+            'Present value of annual energy nominal (kWh)',
+            pv_of_annual_energy_costs_row_name,
+            [
+                pv_of_annual_energy_costs_at_year_0_usd,
+            ],
+        )
+        _insert_blank_line_before(pv_of_annual_energy_costs_row_name)
 
         # Backfill PV of electricity to grid
         electricity_to_grid_backfilled_pv_processed = electricity_to_grid_backfilled.copy()
@@ -404,11 +406,12 @@ class SamEconomicsCalculations:
             for _year in range(len(pv_of_annual_costs_backfilled_row_values_usd)):
                 entry: float | str = 'NaN'
                 if pv_of_electricity_to_grid_backfilled_row_kwh[_year] != 0:
-                    entry = (
-                        pv_of_annual_costs_backfilled_row_values_usd[_year]
-                        * 100
-                        / pv_of_electricity_to_grid_backfilled_row_kwh[_year]
-                    )
+                    pv_annual_costs_usd = (
+                        pv_of_annual_energy_costs_at_year_0_usd
+                        if _year == 0
+                        else pv_of_annual_costs_backfilled_row_values_usd[_year]
+                    )  # FIXME WIP technically inconsistent...
+                    entry = pv_annual_costs_usd * 100 / pv_of_electricity_to_grid_backfilled_row_kwh[_year]
 
                 lcoe_nominal_backfilled.append(entry)
 
