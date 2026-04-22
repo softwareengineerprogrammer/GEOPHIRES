3024ae2004d1b26aa0536acdc9821941d71acb80
softwareengineerprogrammer <4056124+softwareengineerprogrammer@users.noreply.github.com>
2026-04-22 12:42:41 -0700

    additional cases for test_fervo_project_red_2026_results_against_reference_values

--- tests/geophires_x_tests/test_fervo_project_red_2026.py
+++ tests/geophires_x_tests/test_fervo_project_red_2026.py
@@ -1,5 +1,9 @@
 from __future__ import annotations
 
+from typing import Any
+
+from pint.facets.plain import PlainQuantity
+
 from base_test_case import BaseTestCase
 from geophires_x_client import GeophiresInputParameters
 from geophires_x_client import GeophiresXClient
@@ -12,9 +16,23 @@ class FervoProjectCape4TestCase(BaseTestCase):
         Asserts that results conform to some of the key reference values claimed in docs/Fervo_Project_Cape-4.md.
         """
 
+        def _q(v: float, u: str) -> PlainQuantity:
+            return PlainQuantity(v, u)
+
+        def _vuq(v_u: dict[str, Any]) -> PlainQuantity:
+            return self.value_unit_as_quantity(v_u)
+
         r = GeophiresXClient().get_geophires_result(
             GeophiresInputParameters(from_file_path=self._get_test_file_path('../examples/Fervo_Project_Red-2026.txt'))
         )
 
-        avg_pumping_power_mw = r.result['SURFACE EQUIPMENT SIMULATION RESULTS']['Average Pumping Power']['value']
-        self.assertEqual(0.7, avg_pumping_power_mw)
+        self.assertEqual(_q(0.7, 'MW'), _vuq(r.result['SURFACE EQUIPMENT SIMULATION RESULTS']['Average Pumping Power']))
+
+        self.assertLess(_vuq(r.result['SUMMARY OF RESULTS']['Average Net Electricity Production']), _q(2.0, 'MW'))
+
+        self.assertGreater(
+            _vuq(r.result['RESERVOIR SIMULATION RESULTS']['Average Production Temperature']), _q(346, 'degF')
+        )
+        self.assertLess(
+            _vuq(r.result['RESERVOIR SIMULATION RESULTS']['Average Production Temperature']), _q(356, 'degF')
+        )
