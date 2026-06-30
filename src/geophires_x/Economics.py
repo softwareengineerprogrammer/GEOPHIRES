7d1be9f47d0ea2dfb882862ba4f0ef973a56d24a
softwareengineerprogrammer <4056124+softwareengineerprogrammer@users.noreply.github.com>
2026-06-30 10:55:16 -0700

    Reservoir Stimulation Capital Cost per Fracture Surface Area initial param definition and validation - WIP, not yet implemented in cost calculations

--- src/geophires_x/Economics.py
+++ src/geophires_x/Economics.py
@@ -16,7 +16,8 @@ from geophires_x.EconomicsUtils import BuildPricingModel, wacc_output_parameter,
     project_payback_period_parameter, inflation_cost_during_construction_output_parameter, \
     interest_during_construction_output_parameter, total_capex_parameter_output_parameter, \
     overnight_capital_cost_output_parameter, CONSTRUCTION_CAPEX_SCHEDULE_PARAMETER_NAME, \
-    _YEAR_INDEX_VALUE_EXPLANATION_SNIPPET, investment_tax_credit_output_parameter, lcoh_output_parameter, lcoc_output_parameter
+    _YEAR_INDEX_VALUE_EXPLANATION_SNIPPET, investment_tax_credit_output_parameter, lcoh_output_parameter, \
+    lcoc_output_parameter, CALCULATED_PARAMETER_PLACEHOLDER_VALUE
 from geophires_x.ParameterUtils import expand_schedule_dsl
 from geophires_x.GeoPHIRESUtils import quantity
 from geophires_x.OptionList import Configuration, WellDrillingCostCorrelation, EconomicModel, EndUseOptions, PlantType, \
@@ -674,7 +675,7 @@ class Economics:
           self.ParameterDict[self.stimulation_cost_per_production_well.Name] = floatParameter(
             'Reservoir Stimulation Capital Cost per Production Well',
             DefaultValue=stimulation_cost_per_production_well_default_value_MUSD,
-            Min=0,
+            Min=-1,
             Max=max_stimulation_cost_per_well_MUSD,
             UnitType=Units.CURRENCY,
             PreferredUnits=CurrencyUnit.MDOLLARS,
@@ -682,6 +683,22 @@ class Economics:
             ToolTipText=f'Reservoir stimulation capital cost per production well before indirect costs and contingency'
                         f'{stimulation_cost_per_production_well_default_value_note}'
         )
+
+        self.stimulation_cost_per_fracture_surface_area = \
+          self.ParameterDict[self.stimulation_cost_per_fracture_surface_area.Name] = floatParameter(
+            'Reservoir Stimulation Capital Cost per Fracture Surface Area',
+            Min=0,
+            Max=1000,
+            DefaultValue=0.9,
+            UnitType=Units.CURRENCY,
+            PreferredUnits=CurrencyUnit.DOLLARS,
+            CurrentUnits=CurrencyUnit.DOLLARS,
+            # TODO/WIP...
+            ToolTipText=f'Reservoir stimulation capital cost per fracture surface area before indirect costs and '
+                        f'contingency. Set {self.stimulation_cost_per_production_well.Name} = '
+                        f'{CALCULATED_PARAMETER_PLACEHOLDER_VALUE} to indicate that production wells are stimulated.'
+        )
+
 
         self.ccstimadjfactor = self.ParameterDict[self.ccstimadjfactor.Name] = floatParameter(
             "Reservoir Stimulation Capital Cost Adjustment Factor",
@@ -2822,6 +2839,8 @@ class Economics:
                     if sam_em_only_param.Provided:
                         raise NotImplementedError(f'{sam_em_only_param.Name} is only supported for SAM Economic Models')
 
+            self._validate_read_stimulation_cost_parameters(model)
+
         else:
             model.logger.info("No parameters read because no content provided")
 
@@ -2864,6 +2883,34 @@ class Economics:
                 _set_ratio(0.0)
 
         model.logger.info(f'complete {__class__!s}: {sys._getframe().f_code.co_name}')
+
+    def _validate_read_stimulation_cost_parameters(self, model: Model) -> None:
+        def _raise_mutually_exclusive_error(param1: Parameter, param2: Parameter) -> None:
+            raise ValueError(f'Cannot provide both {param1.Name} and {param2.Name} parameters. ')
+
+        if self.stimulation_cost_per_fracture_surface_area.Provided:
+            if self.ccstimfixed.Provided:
+                _raise_mutually_exclusive_error(
+                    self.ccstimfixed,
+                    self.stimulation_cost_per_fracture_surface_area
+                )
+            if self.stimulation_cost_per_injection_well.Provided:
+                _raise_mutually_exclusive_error(
+                    self.stimulation_cost_per_injection_well,
+                    self.stimulation_cost_per_fracture_surface_area
+                )
+            if self.stimulation_cost_per_production_well.Provided and \
+                    self.stimulation_cost_per_production_well.value != \
+                    CALCULATED_PARAMETER_PLACEHOLDER_VALUE:  # Placeholder indicates production wells are stimulated
+                _raise_mutually_exclusive_error(
+                    self.stimulation_cost_per_production_well,
+                    self.stimulation_cost_per_fracture_surface_area
+                )
+        elif self.stimulation_cost_per_production_well.Provided and self.stimulation_cost_per_production_well.value < 0:
+            raise ValueError(f'{self.stimulation_cost_per_production_well.Name} must be positive')
+
+        # TODO validate fixed stimulation cost param mutual exclusivity with stim cost per well params (warn instead of
+        #  raising error for backwards compatibility where applicable)
 
     def sync_interest_rate(self, model):
         def discount_rate_display() -> str:
