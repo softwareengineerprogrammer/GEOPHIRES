0aba13859c616fe4484a3ed69cedc18f7c1d1fab
Bernard Che Ngu <ngubernard1986@gmail.com>
2026-09-18 17:24:46 -0500

    Skip plt.show() on non-interactive backends (#365)

--- src/geophires_x/MatplotlibUtils.py
+++ src/geophires_x/MatplotlibUtils.py
@@ -11,12 +11,24 @@ import logging
 import os
 from typing import Any
 
+import matplotlib
 from matplotlib import pyplot as plt
 
 _logger = logging.getLogger(__name__)
 
 
+_NON_INTERACTIVE_BACKENDS = frozenset({'agg', 'cairo', 'pdf', 'pgf', 'ps', 'svg', 'template'})
+
+
+def _is_non_interactive_backend() -> bool:
+    return matplotlib.get_backend().lower() in _NON_INTERACTIVE_BACKENDS
+
+
 def plt_show(**kw_args):
+    if _is_non_interactive_backend():
+        _logger.debug(f'Skipping plt.show() - non-interactive backend ({matplotlib.get_backend()})')
+        return
+
     try:
         plt.show(**kw_args)
     except Exception as e:
